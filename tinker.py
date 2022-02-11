850db27c5cdf7f6a13a8585957fc5c9bd24da4e2
yo <correa2025@gmail.com>
2022-02-11 17:05:13 +0100

    Cleaned tinker.py class

--- tinker.py
+++ tinker.py
@@ -14,13 +14,6 @@ def add_path_for_modules():
     if not dir in sys.path:
         sys.path.append(dir)
 
-
-def super_market_lights():
-    for x in range(0, 4, 2):
-        for y in np.arange(0, 5, 1.5):
-            light.Light(type='SPOT', location=(x, y, 2))
-
-
 add_path_for_modules()
 import utils
 import generation
@@ -29,37 +22,32 @@ from generation import Render, RenderPreloadingAssets
 import camera
 import light
 
+# Necesary for development so when modules are changed are reloaded in Blender
 importlib.reload(utils)
 importlib.reload(generation)
 importlib.reload(camera)
 importlib.reload(light)
 
+# Have to enable the module for importing images as planes
 addon_utils.enable("io_import_images_as_planes")
 
-# Have to enable the module for importing images as planes
-# https://blender.stackexchange.com/questions/121980/how-to-load-import-a-png-image-as-plane-via-blenders-python-api
-# bpy.ops.wm.addon_enable(module='io_import_images_as_planes')
-
-EPOCHS = 50
+##### CONFIGURATION #####
+EPOCHS = 10
 dataset_path = "/home/yo/Desktop/Desarrollo/blender/Data-Generation-with-Blender/Resources/dataset/"
-cart_path = dataset_path + "cart"
-human_path = dataset_path + "human"
-background_path = dataset_path + "bg"
+render_img_saving_path = '/home/yo/Desktop/Desarrollo/blender/Data-Generation-with-Blender/Resources/generated'
+##### END CONFIGURATION #####
 
-classes = {"cart": 0, "human": 1}
-
-axis = bpy.data.objects['Main Axis']
-axis.rotation_euler = (0, 0, 0)
-axis.location = (0, 0, 0)
-
-
+# camera
 camera = camera.Camera(location=(0, 6, 0.3), location_range=((-0.5,0.5), (4, 6), (0.3, 0.8)), rotation=(90, 0, 180))
 
-render_img_saving_path = '/home/yo/Desktop/Desarrollo/blender/Data-Generation-with-Blender/Resources/generated'
-
+# floor and background object
 floor = generation.Floor(random=True, size=30, location=(0, 0, 0))
-# floor.add_simple_floor()
 
+background = generation.Background(dataset_path, wanted_classes=["bg"],
+                                   location=(0, 0, 0.5), rotation=(m.radians(90), 0, m.radians(180)),
+                                   scale=5)
+
+# Lights objects
 light1 = light.Light(location=(1,1,2), location_range=((-2, 2), (0, 2), (1.2, 3)),
                      energy_range=(60, 200))
 
@@ -67,45 +55,11 @@ light2 = light.Light(location=(1, 1, 2), location_range=(
     (-3, 3), (0, 3), (1.2, 2)),
     energy_range=(60,120))
 
-
-# super_market_lights()
-
-# from timeit import Timer
-
+# we specify which folders are out img data classes
 wanted_objects = ["cart", "human"]
-# wanted_objects = ["cart_simple"]
+render = Render(dataset_path, render_img_saving_path,
+                wanted_objects=wanted_objects, camera=camera)
 
-# render = generation.RenderPreloadingAssets(
-#     dataset_path, render_img_saving_path, camera=camera, wanted_objects=wanted_objects)
-
-background = generation.Background(dataset_path, wanted_classes=["bg"],
-                                   location=(0, 0, 0.5), rotation=(m.radians(90), 0, m.radians(180)),
-                                   scale=5)
-
-# # t = Timer(lambda: render.generate_scene(1))
-# # # print(t.timeit(number=20))  # 22.929327520003426
-# # print(t.timeit(number=5))
-# for i in range(EPOCHS):
-#     # floor.add_simple_floor()
-#     background.add_img_background()
-#     render.generate_scene(i)
-#     camera.randomize_location()
-#     camera.randomize_rotation()
-#     # floor.delete_existing_floor()
-#     background.remove_img_background()
-# render.delete_all_images()
-# camera.delete_camera()
-
-######################
-
-# background.add_img_background()
-# floor.add_simple_floor()
-# background.remove_img_background()
-
-render = Render(dataset_path, render_img_saving_path, wanted_objects=wanted_objects, camera=camera)
-# # t = Timer(lambda: render.generate_scene(1))
-# # print(t.timeit(number=20)) # 22.532155615001102
-# # print(t.timeit(number=5))
 for i in range(EPOCHS):
     # floor and background
     floor.add_simple_floor()
@@ -126,10 +80,3 @@ for i in range(EPOCHS):
 camera.delete_camera()
 light1.delete_light()
 light2.delete_light()
-
-"""
-To get size of an image
-bpy.data.images['1.png'].size
-"""
-
-
